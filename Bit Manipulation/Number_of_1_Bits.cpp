34573e54e9f39e2a5dd9651f70227f1b42a4f691
satyum <kingsatyam7@gmail.com>
2021-07-06 03:18:42 +0000

    Create Number_of_1_Bits.cpp

---
+++ Bit Manipulation/Number_of_1_Bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count=0;
+        while(n)
+        {   
+            if(n>0)
+            {
+                count++;
+                n=n&(n-1);
+            }
+        }
+        return count;
+    }
+};
